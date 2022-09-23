12ec6affbf4e6e1fa983c00995539039382fd01e
Tim Benniks <tbenniks@gmail.com>
2022-09-23 11:17:06 +0200

    cleaned up repo

--- lib/context/uniformContext.ts
+++ lib/context/uniformContext.ts
@@ -8,22 +8,12 @@ import {
 import { NextCookieTransitionDataStore } from "@uniformdev/context-next";
 import { NextPageContext } from "next";
 import manifest from "./manifest.json";
-import { enableGoogleGtagAnalytics } from "@uniformdev/context-gtag";
-import getConfig from "next/config";
-
-const {
-  publicRuntimeConfig: { gaTrackingId },
-} = getConfig();
 
 export function createUniformContext(serverContext?: NextPageContext) {
   const plugins: ContextPlugin[] = [
     enableContextDevTools(),
     enableDebugConsoleLogDrain("debug"),
   ];
-
-  if (gaTrackingId) {
-    plugins.push(enableGoogleGtagAnalytics({ emitAll: true }));
-  }
 
   const context = new Context({
     defaultConsent: true,

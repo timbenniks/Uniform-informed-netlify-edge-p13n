fc47e23165f8a5e53d81400469af84f7b7d62089
Tim Benniks <tbenniks@gmail.com>
2022-09-23 14:12:07 +0200

    easier styling for p13n

--- components/Yolo.tsx
+++ components/Yolo.tsx
@@ -1,6 +1,14 @@
 const Yolo = (props: any) => {
   return (
-    <div style={{ padding: "20px 100px", position: "sticky", top: 0 }}>
+    <div
+      style={{
+        padding: "0 100px",
+        background: "rgba(255, 255, 255, 0.8)",
+        position: "fixed",
+        top: 0,
+        width: "100%",
+      }}
+    >
       <p style={{ fontSize: "100px", fontWeight: "bold" }}>{props.title}</p>
     </div>
   );

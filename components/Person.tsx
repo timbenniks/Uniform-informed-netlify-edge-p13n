536dd0553226ece850a46bdb4cfba470f2afc3d8
Tim Benniks <tbenniks@gmail.com>
2022-09-23 14:25:33 +0200

    yolo

--- components/Person.tsx
+++ components/Person.tsx
@@ -1,6 +1,7 @@
 const Person = (props: any) => {
   return (
     <div style={{ padding: "0 100px" }}>
+      <div style={{ height: "1000px" }}>&nbsp;</div>
       <p style={{ fontSize: "50px", fontWeight: "bold" }}>{props.title}</p>
       <img src={props.image} />
       <div style={{ height: "1000px" }}>&nbsp;</div>

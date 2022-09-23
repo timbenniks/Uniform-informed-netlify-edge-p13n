bc3cc636ceefce4cc53bbf174ce58ab4cd48e79f
Tim Benniks <tbenniks@gmail.com>
2022-09-23 14:14:58 +0200

    easier styling for p13n

--- components/Person.tsx
+++ components/Person.tsx
@@ -1,6 +1,6 @@
 const Person = (props: any) => {
   return (
-    <div style={{ padding: "500px 100px" }}>
+    <div style={{ position: "relative", top: "1000px", padding: "0 100px" }}>
       <p style={{ fontSize: "50px", fontWeight: "bold" }}>{props.title}</p>
       <img src={props.image} />
     </div>
